2c4865b171c3d01fc58fe1a24123d8b5962ba77c
chessco <chesssco@gmail.com>
2020-08-11 19:18:01 -0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:edelivery/src/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+import 'package:edelivery/src/bloc/provider.dart';
+
+import 'package:edelivery/src/pages/home_page.dart';
+import 'package:edelivery/src/pages/producto_page.dart';
+import 'package:edelivery/src/pages/registro_page.dart';
+import 'package:edelivery/src/preferencias_usuario/preferencias_usuario.dart';
+
+void main() async {
+  //for issue related to the upgrade
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final prefs = new PreferenciasUsuario();
+  await prefs.initPrefs();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final prefs = new PreferenciasUsuario();
+    print(prefs.token);
+    return Provider(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Delivery',
+        initialRoute: 'login',
+        routes: {
+          'login': (BuildContext context) => LoginPage(),
+          'registro': (BuildContext context) => RegistroPage(),
+          'home': (BuildContext context) => HomePage(),
+          'producto': (BuildContext context) => ProductoPage(),
+        },
+        theme: ThemeData(
+          primaryColor: Color(0xFF17191C),
+          brightness: Brightness.light,
+          textTheme: TextTheme(
+            title: TextStyle(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          cursorColor: Colors.white,
+          inputDecorationTheme: InputDecorationTheme(
+            border: InputBorder.none,
+            enabledBorder: InputBorder.none,
+            focusedBorder: InputBorder.none,
+          ),
+          fontFamily: 'AvenirLTStd',
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/src/shared/shared.dart
@@ -0,0 +1,6 @@
+export 'package:edelivery/src/shared/background/background.dart';
+export 'package:edelivery/src/shared/buttons/back_button.dart';
+export 'package:edelivery/src/shared/buttons/button.dart';
+export 'package:edelivery/src/shared/buttons/fb_button.dart';
+export 'package:edelivery/src/shared/buttons/twitter_button.dart';
+export 'package:edelivery/src/shared/inputs/textfield.dart';
